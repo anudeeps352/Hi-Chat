27409a5cf7e47772e6646be860e4a6f8c99e46b1
Anudeep <anudeeps352@gmail.com>
2023-07-02 21:41:21 +0530

    backend work

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-  PORT,
+  PORT,"0.0.0.0",
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
