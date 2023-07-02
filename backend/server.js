916b3de8608ef0f9a10a76f1cb7d7880f4d467a6
Anudeep <anudeeps352@gmail.com>
2023-07-02 20:22:44 +0530

    backend work

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,9 @@ app.use("/api/message", messageRoutes);
 //--------deploy--
 
 // Error Handling middlewares
+app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
 app.use(notFound);
 app.use(errorHandler);
 
