766981d28cfb96a6243ed8e996615cbce4602f76
Anudeep <anudeeps352@gmail.com>
2023-07-03 15:35:41 +0530

    Done all mods

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 app.use(cors());
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+app.get("/", (req, res) => {
+  res.send("API Running!");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
